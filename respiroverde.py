b35f365120b305355d9cdf280c09fde222aa5526
lindmendes20-blip <lind.mendes20@gmail.com>
2025-12-27 14:16:56 -0400

    Fix margin styles for headers and paragraphs
    Adjusted margin styles for various elements to improve layout consistency.

--- respiroverde.py
+++ respiroverde.py
@@ -8,12 +8,13 @@ html_code = """
                 <ul style="display:flex;justify-content:space-between;gap:2rem;align-items:center;margin:2rem;font-size:1.5em;list-style:none;">
                     <li><a href="#" style="color:#E9F9B2;text-decoration-line:none;">Inicio </a></li>
                     <li><a href="#" style="color:#E9F9B2;text-decoration-line:none;">Sobre nós</a></li>
+					<li><a href="#" style="color:#E9F9B2;text-decoration-line:none;">Calculadora</a></li>
                 </ul>
             </nav>
         </header>
     </div>
     <div style="background-color:#E9F9B2;">
-        <h1 style="font-size:3rem;text-align:center;text-transform:uppercase;color:#425C00;">A importância da luz para as plantas</h1>
+        <h1 style="font-size:3rem;text-align:center;text-transform:uppercase;color:#425C00; margin-top: 0rem;">A importância da luz para as plantas</h1>
 	<p style="font-size:1.5rem;text-align:justify;color:#425C00;margin:2rem;">
                     Fotossíntese: A luz é essencial para a fotossíntese, o processo pelo qual as plantas convertem a luz solar em energia química. 
                     Durante a fotossíntese, as plantas absorvem dióxido de carbono (CO2) do ar e água do solo, 
@@ -23,16 +24,16 @@ html_code = """
                     Proporciona conforto e bem-estar: Um jardim pode criar um ambiente relaxante e refrescante, 
                     ideal para momentos de descontração.
         </p>
-        <p style="font-size:1.5rem;text-align:justify;color:#425C00;margin:2rem">
+        <p style="font-size:1.5rem;text-align:justify;color:#425C00;margin:2rem;">
                     Melhora a decoração: Plantas podem adicionar um toque natural à decoração do apartamento, 
                     criando um ambiente mais harmonioso.
         </p>
-        <p style="font-size:1.5rem;text-align:justify;color:#425C00;margin:2rem">
+        <p style="font-size:1.5rem;text-align:justify;color:#425C00;margin:2rem; margin-bottom: 0rem;">
                     Cria um espaço funcional: Um jardim pode ser utilizado para refeições, relaxamento ou simplesmente para apreciar a natureza.
         </p>
     </div>
     <div style="background-color:#425C00;">
-        <h1 style="font-size:3rem;text-align:center;text-transform:uppercase;color:#FFFFFF; margin:2rem; margin-top:0rem">Diferentes tipos de necessidade de luz?</h1>
+        <h1 style="font-size:3rem;text-align:center;text-transform:uppercase;color:#FFFFFF; margin:2rem;margin-top: 0rem;">Diferentes tipos de necessidade de luz?</h1>
         <p style="font-size:1.5rem;text-align:justify;color:#FFFFFF;margin:2rem;">
                     Plantas que necessitam de sol direto: Preferem várias horas de luz intensa ao dia, como cactos e suculentas. 
 	Eles prosperam sob luz forte e podem sofrer com falta de luminosidade.
